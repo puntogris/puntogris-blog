2b9191f9f552b58cdffbe76a245ddbdd20bd8824
puntogris <rosariodota3@gmail.com>
2023-01-26 23:03:17 -0300

    added search input placeholder

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -71,6 +71,7 @@ function Test({ setValue }) {
         <input
           class="placeholder:italic text-slate-800 placeholder:text-slate-500 block bg-slate-100 w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
           type="text"
+          placeholder="Search anything"
           onInput={setValue}
         />
       </label>
