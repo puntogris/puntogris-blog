0e6a6abe46dceecfe06fad267223241021e9a784
puntogris <rosariodota3@gmail.com>
2023-01-26 23:26:10 -0300

    Search code refactoring

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,56 +3,33 @@ import Fuse from "fuse.js";
 import PostCard from "./PostCard";
 
 export default function Search({ searchList }) {
-  const [input, setInput] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   const [searchResults, setSearchResults] = useState();
 
-  const onChangeHandler = (event) => {
-    setInput(event.target.value);
-  };
-
   const fuse = new Fuse(searchList, {
-    keys: ["title", "description", "headings"],
+    keys: ["title", "description", "headings", "tags"],
     includeMatches: true,
     minMatchCharLength: 2,
     threshold: 0.5,
   });
 
   useEffect(() => {
-    const search = fuse.search(input);
-    console.log(search);
-
+    const search = fuse.search(searchInput);
     setSearchResults(search);
-  }, [input]);
+  }, [searchInput]);
 
   return (
     <div>
-      <Test setValue={onChangeHandler} />
-      {input.length > 1 && (
-        <div className="mt-8 text-gray-300">
-          Found {searchResults?.length}
-          {searchResults?.length && searchResults?.length === 1
-            ? " result"
-            : " results"}{" "}
-          for '{input}'
-        </div>
-      )}
-      <ul>
-        {searchResults &&
-          searchResults.map(({ item, refIndex }) => (
-            <li>
-              <PostCard post={item} />
-            </li>
-          ))}
-      </ul>
+      <SerchInput onInput={(e) => setSearchInput(e.target.value)} />
+      <SearchResults results={searchResults} input={searchInput} />
     </div>
   );
 }
 
-function Test({ setValue }) {
+function SerchInput({ onInput }) {
   return (
     <div>
       <label class="relative block mt-5">
-        <span class="sr-only">Search</span>
         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -72,9 +49,33 @@ function Test({ setValue }) {
           class="placeholder:italic text-slate-800 placeholder:text-slate-500 block bg-slate-100 w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
           type="text"
           placeholder="Search anything"
-          onInput={setValue}
+          onInput={onInput}
         />
       </label>
+    </div>
+  );
+}
+
+function SearchResults({ results, input }) {
+  return (
+    <div>
+      {input.length > 1 && (
+        <div className="mt-8 text-gray-300">
+          Found {results?.length}
+          {results?.length && results?.length === 1
+            ? " result"
+            : " results"}{" "}
+          for '{input}'
+        </div>
+      )}
+      <ul>
+        {results &&
+          results.map(({ item }) => (
+            <li>
+              <PostCard post={item} />
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
