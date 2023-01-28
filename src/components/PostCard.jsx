dbaff47f1085f3921efa2dda6e880f9080f83c68
puntogris <rosariodota3@gmail.com>
2023-01-28 00:24:23 -0300

    postCard code cleanup

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,21 +3,14 @@ export default function PostCard({ post }) {
     <article class="flex bg-gray-800 rounded-lg overflow-auto flex-col xs:flex-row">
       {post.frontmatter.image && (
         <a
-          class="flex basis-0 shrink-0 xs:basis-64 md:basis-96 grow-0 max-w-screen-xs"
+          class="flex h-full object-cover basis-0 shrink-0 grow-0 max-w-screen-xs xs:basis-64 md:basis-96"
           href={post.url}
         >
-          <picture>
-            <source
-              srcset="https://arina.loli.net/2022/12/31/KURW6omZCO8XGA1.jpg/480/400"
-              media="(max-width: 480px)"
-            />
-            <img
-              class="h-full object-cover"
-              src={post.frontmatter.image.url}
-              alt={post.frontmatter.image.alt}
-              loading="lazy"
-            />
-          </picture>
+          <img
+            src={post.frontmatter.image.url}
+            alt={post.frontmatter.image.alt}
+            loading="lazy"
+          />
         </a>
       )}
       <div class="flex flex-col justify-between p-6">
@@ -26,7 +19,7 @@ export default function PostCard({ post }) {
         </a>
         <p class="text-sm text-gray-400">{post.frontmatter.description}</p>
         <div class="flex flex-wrap gap-3 text-gray-300">
-          <div>{post.frontmatter.pubDate}</div>
+          {post.frontmatter.date}
           {post.frontmatter.tags.map((tag) => (
             <a class="hover:text-blue-400" href={`/categories/${tag}`}>
               {tag}
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -1,6 +1,6 @@
 ---
 title: 'aThis is the first post of my new Astro blog.'
-pubDate: '2022-07-01'
+date: '2022-07-01'
 description: 'This is the first post of my new Astro blog. This is the first post of my new Astro blog. This is the first post of my new Astro blog. '
 author: 'Astro Learner'
 tags: ["astro", "blogging", "learning in public"]
