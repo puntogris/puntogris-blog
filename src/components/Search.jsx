319133f291220f86e8ac52e5a7597041b87af3a9
puntogris <rosariodota3@gmail.com>
2023-03-11 01:00:48 -0300

    added auto focus to search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -46,6 +46,7 @@ function SerchInput({ onInput }) {
           </svg>
         </span>
         <input
+          autoFocus
           class="block w-full rounded-md border border-slate-300 bg-slate-100 py-2 pl-9 pr-3 text-slate-800 shadow-sm placeholder:italic placeholder:text-slate-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
           type="text"
           placeholder="Search anything"
