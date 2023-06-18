1410d1f99a756604a11e2cd098dc9961d7d5fa57
puntogris <rosariodota3@gmail.com>
2023-06-17 21:55:52 -0300

    updated rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function get(context) {
+  const posts = await getCollection("blog");
   return rss({
     title: "Puntogris blog",
     description: "Blog about coding, mostly Android stuff",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./posts/*.{md,mdx}")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
   });
 }
